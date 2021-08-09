cf097bf98d13f69bb3c0e21dbe49cd105c738ed1
shreyachandra09 <32015268+shreya-09@users.noreply.github.com>
2021-08-09 10:08:01 +0000

    day 1 project

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print ("hello world")
-print ("Day 1 - Python print function")
-print ("The function is declared like this : ")
-print ("print('what to print')")
+#1. Create a greeting for your program.
+print("hello user!")
+#2. Ask the user for the city that they grew up in.
+CityName = input("What is the name of the city where you grew up?\n")
+#3. Ask the user for the name of a pet.
+PetName = input("what is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+BandName = (CityName + " " + PetName)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
+print(BandName)
